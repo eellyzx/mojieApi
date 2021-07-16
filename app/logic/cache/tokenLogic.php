d7552f393e65753e8aa7268b52aa94ae23660ce1
zengxiong <570800818@qq.com>
2021-07-16 16:40:08 +0800

    缓存

---
+++ app/logic/cache/tokenLogic.php
@@ -0,0 +1,19 @@
+<?php
+namespace app\logic\cache;
+
+/**
+ * token
+ * Class tokenLogic
+ * @package app\logic\cache
+ */
+class tokenLogic
+{
+    /**
+     * token装缓存key
+     * @param $token
+     */
+    public function tokenToCache($token)
+    {
+        
+    }
+}
--- config/system.php
+++ config/system.php
@@ -6,27 +6,18 @@
 return [
     //微信配置
     'wxapp'	=> [
-        'appid'     => 'wx4d298dd26854cec7',
-        'secret'	=> '4feda34d437fbada59fd5c8674d0627e',
+        'appid'     => 'wx0d6461d32f04ce2f',
+        'secret'	=> '32b24c46a89767c0541f3912f748ae5a',
     ],
     //qq配置
     'qqapp' => [
-        'appid'  => '1111942495',
-        'secret' => 'CTyMHgaE8mdy2hME'
+        'appid'  => '',
+        'secret' => ''
     ],
     //接口秘钥
     'api' => [
         'key'     => '631ge83469qhb73blsfh13mz7sce9zey',
         'secret'  => 'hByI46rxONoLiG7AOFb1hi91hQjjQSLS',
     ],
-    //钉钉告警
-    'webhook' => 'https://oapi.dingtalk.com/robot/send?access_token=62693137cac6a36687ed17fd5044d923c081976b24bf60a1dd4f0f2745543124',
-    'app_name' => '百战恶灵',
-    //是否使用性能通知
-    'use_notice' => true,
-    //接口地址
-    'api_url' => 'http://test-xcx22.youlingba.com',
-    //图片地址
-    'pic_url' => 'http://test-p22.youlingba.com/',
     'gateway_register_id' => '127.0.0.1:1236',
 ];
